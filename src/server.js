fc5a6b84a81c2c9555cb9468013c05d9ee567d0b
sintell <a.anatskiy@hh.ru>
2019-12-24 19:55:31 +0300

    formatting

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
 import * as sapper from '@sapper/server';
+import compression from 'compression';
+import polka from 'polka';
+import sirv from 'sirv';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = polka().use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware());
+const app = polka().use(
+  compression({ threshold: 0 }),
+  sirv('static', { dev }),
+  sapper.middleware()
+);
 
 export default app.handler;
 
 if (!process.env.NOW_REGION) {
   app.listen(PORT, err => {
-    if (err) console.log('error', err);
+    if (err) console.error('error', err);
   });
 }
