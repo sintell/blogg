49260141e20b5f13b3562cc961a2f19e671570e4
sintell <a.anatskiy@hh.ru>
2019-12-25 03:51:13 +0300

    restore missing Quote component

--- src/components/Quote.svelte
+++ src/components/Quote.svelte
@@ -2,3 +2,11 @@
   import Text from './Text.svelte';
   export let textSections = [];
 </script>
+
+<div
+  class="-mx-5 pl-5 md:-mx-32 md:pl-32 lg:-mx-56 lg:pl-56 bg-gray-200 border-b-4
+  border-gray-400">
+  <p class="w-5/6 md:w-full py-4 m-0 text-lg md:text-base">
+    <Text {textSections} />
+  </p>
+</div>
