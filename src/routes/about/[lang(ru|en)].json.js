0134d61c112380f0359877164030fbc1928e26df
sintell <a.anatskiy@hh.ru>
2019-12-27 03:53:00 +0300

    fix const name

--- src/routes/about/[lang(ru|en)].json.js
+++ src/routes/about/[lang(ru|en)].json.js
@@ -1,12 +1,11 @@
 import { getNotionPageContent } from '../../data/notion';
 
-const ABOUT_RU_PAEGE_ID = '10d7e394-666e-4c1d-9617-0465114ff7b5';
+const ABOUT_RU_PAGE_ID = '10d7e394-666e-4c1d-9617-0465114ff7b5';
 const ABOUT_EN_PAGE_ID = '457419ef-2f00-4e99-b761-5c3c827688e0';
 
 // [lang([ruen]{2})].json
 export async function get(req, res, next) {
-  const pageId =
-    req.params.lang === 'ru' ? ABOUT_RU_PAEGE_ID : ABOUT_EN_PAGE_ID;
+  const pageId = req.params.lang === 'ru' ? ABOUT_RU_PAGE_ID : ABOUT_EN_PAGE_ID;
   try {
     const data = await getNotionPageContent(pageId);
     if (data) {
