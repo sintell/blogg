9f0ce0b0a1f45b80235f4a3c6b9754665051ab23
sintell <a.anatskiy@hh.ru>
2019-12-27 03:47:44 +0300

    make taglist component

---
+++ src/components/Tag.svelte
@@ -0,0 +1,8 @@
+<script>
+  let color = 'gray';
+  export { color };
+</script>
+
+<span class="bg-{color}-200 p-2 mr-2 text-xs rounded-sm">
+  <slot />
+</span>
---
+++ src/components/TagList.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Tag from './Tag.svelte';
+  let items = [];
+  export { items };
+</script>
+
+<div class="mt-1 mb-5">
+  {#each items as tag}
+    <Tag>{tag}</Tag>
+  {/each}
+</div>
